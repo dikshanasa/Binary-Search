e80e30f1623a4b67a5a5237b9dc085d4b72a70c3
Diksha Nasa <64835443+dikshanasa@users.noreply.github.com>
2022-01-21 23:38:01 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<array>
+using namespace std;
+
+int main()
+{
+int n;
+cin>>n;
+int arr[n];
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+}
+sort(arr,arr+n);
+int key;
+cin>>key;
+int l=0;
+int h=n;
+int mid;
+while(l<=h){
+    mid=(l+h)/2;
+    if(key==arr[mid]){
+        cout<<"Found at position: "<<mid;
+        return 0;
+    }
+    else if (key <arr[mid]){
+        mid=h-l;
+    }
+    else{
+        mid=h+l;
+    }
+}
+cout<<"not found";
+return 0;
+}
